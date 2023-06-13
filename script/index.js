57a2f9108c51be1222ed7c8f1b8373812dc86674
https://github.com/marija-rajak <mrajchica@gmail.com>
2023-06-13 22:35:03 +0200

    showcase-design added, photos added to database fonts added

--- script/index.js
+++ script/index.js
@@ -11,7 +11,7 @@ function fillShowcase() {
 	const fragment = document.createDocumentFragment();
 	showcaseProds.forEach(function (product) {
 		const productFrame = document.createElement('div');
-		productFrame.classList.add('product')
+		productFrame.classList.add('product');
 
 		const productImage = document.createElement('img');
 		productImage.src = product.image;
@@ -19,37 +19,44 @@ function fillShowcase() {
 		productImage.classList.add('image');
 		productFrame.appendChild(productImage);
 
+		const productData = document.createElement('div');
+		productData.classList.add('data');
+
 		const productTitle = document.createElement('h3');
 		productTitle.innerText = product.title;
 		productTitle.classList.add('title');
-		productFrame.appendChild(productTitle);
+		productData.appendChild(productTitle);
 
 		const priceFrame = document.createElement('div');
 		priceFrame.classList.add('price');
-		productFrame.appendChild(priceFrame);
+		productData.appendChild(priceFrame);
 
 		const productPrice = document.createElement('h4');
-		productPrice.innerText = product.price +" RSD";
+		productPrice.innerText = product.price.toLocaleString("sr-Latn-RS", {style: "currency", currency: "RSD", minimumFractionDigits: 2});
 		productPrice.classList.add('actual-price');
 		priceFrame.appendChild(productPrice);
 
 		const addButton = document.createElement('button');
 		addButton.type = 'button';
 		addButton.innerText = 'DODAJ U KORPU';
-		productFrame.appendChild(addButton);
+		productData.appendChild(addButton);
+
+		productFrame.appendChild(productData);
+
+		const discountLabel = document.createElement('div');
 
 		if (product.price !== product.discount_price) {
 			const productDiscount = document.createElement('h4');
-			productDiscount.innerText = product.discount_price;
+			productDiscount.innerText = product.discount_price.toLocaleString("sr-Latn-RS", {style: "currency", currency: "RSD", minimumFractionDigits: 2});
 			productDiscount.classList.add('actual-price');
 			productPrice.classList.add('previous-price');
 			priceFrame.appendChild(productDiscount);
 
-			const discountLabel = document.createElement('div');
 			discountLabel.innerText = 'AKCIJA!';
 			discountLabel.classList.add('label');
-			productFrame.appendChild(discountLabel);
 		}
+		productFrame.appendChild(discountLabel);
+
 		fragment.append(productFrame);
 	});
 	showcase.appendChild(fragment);
@@ -62,7 +69,10 @@ function getProducts() {
 			return response.json()
 		})
 		.then(function (products) {
-			showcaseProds = products;
+			while (showcaseProds.length < 8) {
+				const a = Math.floor(Math.random() * (products.length));
+				showcaseProds.push(products.splice(a, 1)[0]);
+			}
 		})
 		.then(function () {
 			fillShowcase();
